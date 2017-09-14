b307fe03576e4b2fb7fc7fc5b40eec477189fe47
BT_1N3_11 <BT_1N3_11@BT_1N3_11.bktech.local>
2017-09-14 14:39:31 -0400

    a

---
+++ KevinLiu9/src/duel/CharacterA.java
@@ -0,0 +1,16 @@
+package duel;
+
+public class CharacterA implements Dueler 
+{
+	public CharacterA()
+	{
+		public void taunt() 
+		{
+			
+		}
+		public String getName
+		{
+			
+		}
+	}
+}
---
+++ KevinLiu9/src/duel/Duel.java
@@ -0,0 +1,58 @@
+package duel;
+
+public class Duel 
+{
+	private Dueler d1;
+	private Dueler d2;
+	private int dueler1HP = 30;
+	private int dueler2HP = 30;
+	
+	
+	
+	
+	public static void main(String[] args)
+	{
+		Duel duel = new Duel();
+		duel.dukeItOut();
+	}
+	
+	
+	
+	
+	public Duel() 
+	{
+		d1 = new CharacterA();
+		d2 = new CharacterB();
+	}
+	
+	
+	
+	
+	public void dukeItOut()
+	{
+		System.out.print(d1.getName());
+		pause(600);
+		System.out.print(" vs ");
+		pause(600);
+		System.out.println(d2.getName());
+		pause(600);
+		d1.taunt();
+		pause(2000);
+		d2.taunt();
+		pause(2000);
+		d1.setStartingHP(dueler1HP);
+		d2.setStartingHP(dueler2HP);
+	}
+	
+	
+	
+	
+	public void pause(long time){
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
