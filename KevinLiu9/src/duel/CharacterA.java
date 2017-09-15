9538c404ae39def04690358191b4f974abf104c7
BT_1N3_11 <BT_1N3_11@BT_1N3_11.bktech.local>
2017-09-15 15:34:56 -0400

    a

--- KevinLiu9/src/duel/CharacterA.java
+++ KevinLiu9/src/duel/CharacterA.java
@@ -1,6 +1,6 @@
 package duel;
 
-public class CharacterA implements Dueler 
+public class CharacterA extends Duel implements Dueler 
 {
 	private String name;
 	private int hp;
@@ -21,6 +21,28 @@ public class CharacterA implements Dueler
 	}
 	public void setStartingHP(int hp)
 	{
-		System.out.
+		System.out.println(hp);
+	}
+	public int getHP()
+	{
+		return hp;
+	}
+	public boolean determineIfOpponentIsFair(Dueler d, int hp)
+	{
+		if (d.hp == 100)
+		{
+			return true;
+		}
+	}
+	public int getAction(Object caller)
+	{
+		return caller.;
+	}
+	public void hit(Object caller)
+	{
+		if (caller instanceof Duel)
+		{
+			hp = hp - 10;
+		}
 	}
 }
